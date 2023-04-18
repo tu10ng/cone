193142471b1c1e67518d0cad24bab5b140765d43
tu10ng <2059734099@qq.com>
2023-04-18 23:19:24 +0800

    error

--- cone.py
+++ cone.py
@@ -3,15 +3,17 @@ import numpy as np
 import math
 
 
-class Bend(Animation):
-    def __init__(self, sector: Sector, r0, theta, axes, **kwargs) -> None:
-        super().__init__(sector, **kwargs)
+class Bend:
+    def __init__(
+        self, sector: Sector, r0: float, theta: float, axes: ThreeDAxes
+    ) -> None:
+        self.sector = sector
         self.r0 = r0
         self.theta = theta
         self.axes = axes
 
-    def recalc_cone(self, r, h, t):
-        coneSurface = Surface(
+    def recalc_cone(self, h, t, r):
+        cone = Surface(
             lambda u, v: self.axes.c2p(
                 v,
                 r * v * np.cos(u) / h,
@@ -22,28 +24,39 @@ class Bend(Animation):
             checkerboard_colors=[YELLOW_B, YELLOW_B],
             stroke_color=YELLOW,
         )
-
-        return coneSurface
+        return cone
 
     def recalc_htr(self, alpha):
         """
         some magic calculating how to update the drawing parameters
         """
-        h = self.r0 - (1 - math.sqrt(4 * PI * PI - self.theta * self.theta) / (2 * PI)) * self.r0 * alpha
+        if alpha < 0.1:
+            alpha = 0.1
+        h = (
+            self.r0
+            - (1 - math.sqrt(4 * PI * PI - self.theta * self.theta) / (2 * PI))
+            * self.r0
+            * alpha
+        )
         t = 2 * PI * alpha
         r = math.sqrt(self.r0 * self.r0 - h * h) / (1 - math.cos(t / 2))
         return h, t, r
 
-    def interpolate_mobject(self, alpha: float) -> None:
-        h, t, r = self.recalc_htr(alpha)
-        cone = self.recalc_cone(h, t, r)
-        self.mobject.set_value(cone)
+    def animation(self, scene) -> None:
+        tmp = []
+        rate = 10
+        for alpha in [i / rate for i in range(1, rate + 1)]:
+            h, t, r = self.recalc_htr(alpha)
+            cone = self.recalc_cone(h, t, r)
+            tmp.append(cone)
+
+        scene.play(Transform(self.sector, tmp[0], rate_func=linear, run_time=1/rate))
+        for i, j in zip(tmp, tmp[1:]):
+            scene.play(Transform(i, j), rate_func=linear, run_time=1/rate)
 
 
 class anim(ThreeDScene):
     def construct(self):
-        # polygon = Polygon()
-        # self.add(polygon)
         axes = ThreeDAxes(x_range=[-6, 6, 1], y_range=[-6, 6, 1], z_range=[-6, 6, 1])
         self.set_camera_orientation(phi=PI / 4, theta=PI / 4)
         self.add(axes)
@@ -54,17 +67,22 @@ class anim(ThreeDScene):
         sector = Sector(r0).rotate(-PI / 4, about_point=ORIGIN)
         sector.set_fill(WHITE, opacity=0.3)
         self.add(sector)
+        self.wait()
+
+        bend = Bend(sector, r0, theta, axes)
+        bend.animation(self)
+        self.wait()
 
         # alpha = 0.1
         # h, t, r = recalc_htr(alpha, r0, theta)
         # coneSurface = recalc_cone(axes, r, h, t)
         # self.add(coneSurface)
         # self.play(Transform(sector, coneSurface))
-        tmp = []
-        for alpha in [i / 10 for i in range(1, 11)]:
-            h, t, r = recalc_htr(alpha, r0, theta)
-            cone = recalc_cone(axes, r, h, t)
-            tmp.append(cone)
+        # tmp = []
+        # for alpha in [i / 10 for i in range(1, 11)]:
+        #     h, t, r = recalc_htr(alpha, r0, theta)
+        #     cone = recalc_cone(axes, r, h, t)
+        #     tmp.append(cone)
 
-        for i, j in zip(tmp, tmp[1:]):
-            self.play(Transform(i, j))
+        # for i, j in zip(tmp, tmp[1:]):
+        #     self.play(Transform(i, j))
