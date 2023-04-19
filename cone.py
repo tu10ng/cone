dcec87f8263afa6c3a04f29ba5edcd57c9bd3f3d
tu10ng <2059734099@qq.com>
2023-04-19 10:55:56 +0800

    commit by tu10ng in emacs@2023-04-19 10:55:56

--- cone.py
+++ cone.py
@@ -12,20 +12,6 @@ class Bend:
         self.theta = theta
         self.axes = axes
 
-    def recalc_cone(self, h, t, r):
-        cone = Surface(
-            lambda u, v: self.axes.c2p(
-                v,
-                r * v * np.cos(u) / h,
-                r * v * np.sin(u) / h,
-            ),
-            u_range=[0, t],
-            v_range=[0, h],
-            # checkerboard_colors=[YELLOW_B, YELLOW_B],
-            # stroke_color=YELLOW,
-        )
-        return cone
-
     def recalc_htr(self, alpha):
         """
         some magic calculating how to update the drawing parameters
@@ -42,33 +28,46 @@ class Bend:
         r = math.sqrt(self.r0 * self.r0 - h * h) / (1 - math.cos(t / 2))
         return h, t, r
 
+    def draw_cone(self, alpha):
+        """
+        `always_redraw' needs a function, but i dont know how to embed 2 expr in python's lambda, so we have this function
+        """
+        alpha = alpha.get_value()
+        h, t, r = self.recalc_htr(alpha)
+        cone = Surface(
+            lambda u, v: self.axes.c2p(
+                v,
+                r * v * np.cos(u) / h,
+                r * v * np.sin(u) / h,
+            ),
+            u_range=[0, t],
+            v_range=[0, h],
+        )
+        return cone
+
     def animation(self, scene) -> None:
-        rate = 10
-        time = 3
-        tmp = [i / rate for i in range(1, rate + 1)]
-
-        for alpha0, alpha1 in zip(tmp, tmp[1:]):
-            h, t, r = self.recalc_htr(alpha0)
-            cone0 = self.recalc_cone(h, t, r)
-            h, t, r = self.recalc_htr(alpha1)
-            cone1 = self.recalc_cone(h, t, r)
-            if alpha0 == tmp[0]:
-                scene.play(
-                    Transform(self.sector, cone0), rate_func=linear, run_time=time / rate
-                )
-                scene.remove(cone0)
-                scene.remove(self.sector)
-
-            scene.play(Transform(cone0, cone1), rate_func=linear, run_time=time / rate)
-            scene.remove(cone0)
-            if alpha1 != 1:
-                scene.remove(cone1)
+        alpha = ValueTracker(0)
+        cone = always_redraw(lambda: self.draw_cone(alpha))
+        # scene.play(Transform(self.sector, cone))
+        # scene.wait()
+        scene.add(cone)
+        scene.play(ReplacementTransform(self.sector, cone))
+        scene.play(alpha.animate(run_time=4, rate_func=smooth).set_value(1))
+        
+        h, t, r = self.recalc_htr(1)
+        final_cone = Cone(base_radius=self.axes.c2p(0, r)[1],
+                          height=self.axes.c2p(h, 0)[0],
+                          direction=LEFT,
+                          checkerboard_colors=False,
+                          show_base=True)
+        scene.play(ReplacementTransform(cone, final_cone))
+        scene.wait()
 
 
 class anim(ThreeDScene):
     def construct(self):
         axes = ThreeDAxes(x_range=[-6, 6, 1], y_range=[-6, 6, 1], z_range=[-6, 6, 1])
-        self.set_camera_orientation(phi=PI / 4, theta=PI/4)
+        self.set_camera_orientation(phi=PI / 4, theta=PI / 4)
         self.add(axes)
         self.begin_ambient_camera_rotation()
         self.wait()
@@ -77,7 +76,7 @@ class anim(ThreeDScene):
         theta = PI / 2
 
         sector = Sector(r0)
-        sector.rotate(PI/2, axis=np.array([PI/4, PI/4, 0.0]))
+        sector.rotate(PI / 2, axis=np.array([PI / 4, PI / 4, 0.0]))
         # .rotate(-PI / 4, about_point=ORIGIN)
         sector.set_fill(WHITE, opacity=0.3)
         self.add(sector)
@@ -86,17 +85,3 @@ class anim(ThreeDScene):
         bend = Bend(sector, r0, theta, axes)
         bend.animation(self)
         self.wait(3)
-
-        # alpha = 0.1
-        # h, t, r = recalc_htr(alpha, r0, theta)
-        # coneSurface = recalc_cone(axes, r, h, t)
-        # self.add(coneSurface)
-        # self.play(Transform(sector, coneSurface))
-        # tmp = []
-        # for alpha in [i / 10 for i in range(1, 11)]:
-        #     h, t, r = recalc_htr(alpha, r0, theta)
-        #     cone = recalc_cone(axes, r, h, t)
-        #     tmp.append(cone)
-
-        # for i, j in zip(tmp, tmp[1:]):
-        #     self.play(Transform(i, j))
